5562ecda39f6ee155e7a5bd76ebb5c7ba2986125
Akito Tsutsui <akito.tsutsui@gmail.com>
2019-04-09 16:50:03 +0900

    ActionCableを導入した

--- frontend/components/messages/messages.js
+++ frontend/components/messages/messages.js
@@ -1,3 +1,20 @@
 // frontend/components/messages/messages.js
-import "components/message/message"; // メッセージはネストされるので、ここでimportする
+import { setCallback } from "client/chat";
+import "components/message/message";
 import "./messages.css";
+
+const messages = document.querySelector(".js-messages");
+const content = messages.querySelector(".js-messages--content");
+
+function scrollToBottom() {
+  content.scrollTop = content.scrollHeight;
+}
+
+scrollToBottom();
+
+// ActionCableで新しいメッセージを1件受け取るたびに
+// このコード片を呼び出すよう`chat.js`に伝える
+setCallback(message => {
+  content.insertAdjacentHTML("beforeend", message);
+  scrollToBottom();
+});
